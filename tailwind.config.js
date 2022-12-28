8d0eaf2ce5fe18274330e4956cf1f9b98d104ed6
Ankit Pandey <ankitechzz@gmail.com>
2022-12-28 23:51:14 +0530

    design ready

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,8 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content:['./*.html'],
+  content: ['./*.html'],
   theme: {
-    screens: {
-      sm : '480px',
-      md : '768px',
-      lg : '976px',
-      xl : '1440px'
-    },
-    extend: {
-      colors: {
-        backgroundBlack: '#0A0A0D',
-        gradient: 'linear-gradient(to right, #F06844, #EE4C54, #D45E95, #9C6CA6, #6583C1);'
-         }
-    },
+    extend: {},
   },
   plugins: [],
 }
